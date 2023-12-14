475f82a9f7baef47cc8140bfbf630c90ecf92cd5
Quốc Đạtt <37604706+quocdat1804@users.noreply.github.com>
2023-12-14 14:49:53 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "linphonesw",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
